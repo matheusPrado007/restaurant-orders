9b5a0e0752064b98f63003f224a9eefae4249c45
Prado <peu_prado01@hotmail.com>
2023-06-24 02:42:01 -0300

    test: 1.5 - Será validado que seu teste falha caso a implementação do método __repr__ retorne um valor inadequado.

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -9,3 +9,4 @@ def test_ingredient():
     assert hash(Ingredient("farinha")) != hash(Ingredient("queijo mussarela"))
     assert Ingredient("farinha") == Ingredient("farinha")
     assert Ingredient("farinha") != Ingredient("queijo mussarela")
+    assert repr(Ingredient("bacon")) == "Ingredient('bacon')"
