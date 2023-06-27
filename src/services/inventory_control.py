694e213e5721fc2792fb5c0a9fa192b0519e3d90
Prado <peu_prado01@hotmail.com>
2023-06-27 02:59:03 -0300

    feat: Estoque de ingredientes

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -25,10 +25,20 @@ class InventoryMapping:
     def __init__(self, inventory_file_path=BASE_INVENTORY) -> None:
         self.inventory = read_csv_inventory(inventory_file_path)
 
-    # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        return all(
+            self.inventory.get(ingredient, 0) >= amount
+            for ingredient, amount in recipe.items()
+        )
 
-    # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not self.check_recipe_availability(recipe):
+            raise ValueError("A receita não está disponível para consumo")
+
+        self.inventory.update(
+            {
+                ingredient: self.inventory.get(ingredient) - amount
+                for ingredient, amount in recipe.items()
+                if ingredient in self.inventory
+            }
+        )
